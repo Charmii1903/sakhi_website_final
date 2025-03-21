5a9504ba1011a0f145794f5fc8ef3b0ec1cb63a8
charmi <charmisolanki1907@gmail.com>
2025-03-21 15:38:00 +0530

    fro url

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // ✅ Fix CORS Issues
 const allowedOrigins = [
-    'https://sakhi-frontendd.vercel.app',
+    'https://sakhi-frontend-trn8.vercel.app/',
     'https://sakhi-adminn.vercel.app'  // Ensure admin panel is allowed
 ];
 
